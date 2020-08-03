b516f0505c21ccc863f829cfe06dee0836448731
karim oueslati <karimoueslati16@gmail.com>
2020-08-03 15:37:06 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+  <h2>Formulaire de contact</h2>
+   <form class="form-inline" >
+    <div class="form-group">
+    <label for="Name">Name:</label>
+      <input type="name" class="form-control" id="name" placeholder="Enter name" name="name"/>
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
+    </div>
+    
+   </form> 
+</div>
     </div>
   );
 }
